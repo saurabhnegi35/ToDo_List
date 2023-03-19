b45a874cd9a7ea20d8f3f7db9538b5d4d12ebc7e
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-03-19 16:38:00 +0530

    final commit

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-port = 8000;
-const expressLayout = require('express-ejs-layouts');
+const port = process.env.PORT || 8000;
+const expressLayout = require("express-ejs-layouts");
 app.use(expressLayout);
-const db = require('./config/mongoose');
-
+const db = require("./config/mongoose");
 
 // it is use to handle middleware here we are using express.urlencoded to use the parser
 app.use(express.urlencoded());
-app.use('/',require('./routes'));
-app.use(express.static('./assets')); // for getting static
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true)
-app.set('view engine','ejs');
-app.set('views','./views');
+app.use("/", require("./routes"));
+app.use(express.static("./assets")); // for getting static
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error in Running the Server on Port ${port}`);
-        return;
-    }
-    console.log(`Server running on Port: ${port}`);
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in Running the Server on Port ${port}`);
+    return;
+  }
+  console.log(`Server running on Port: ${port}`);
 });
